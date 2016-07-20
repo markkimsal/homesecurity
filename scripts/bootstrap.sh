966ad12a70730bd484287b2f0ef4b78fd0fa4eaa
Mark Kimsal <metrofindings@gmail.com>
2016-07-20 11:40:37 -0400

    Add script to download Arduino-Makefile

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -O Arduino-Makefile-1.5.1.tar.gz 'https://github.com/sudar/Arduino-Makefile/archive/1.5.1.tar.gz'
+tar -zxvf Arduino-Makefile-1.5.1.tar.gz
+mv Arduino-Makefile-1.5.1 Arduino-Makefile
+rm Arduino-Makefile-1.5.1.tar.gz
